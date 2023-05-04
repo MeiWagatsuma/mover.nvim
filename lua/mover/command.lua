b0cd4ba9e2955d87cf9ab5ae47f3dcd20da74176
MeiWagatsuma <wagamei256@gmail.com>
2023-05-04 12:28:18 +0900

    Rename functional file

--- lua/mover/command.lua
+++ lua/mover/command.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.init()
-	vim.api.nvim_create_user_command("Mover", require("mover.notify"), {})
+	vim.api.nvim_create_user_command("Mover", require("mover.move"), {})
 end
 
 return M
